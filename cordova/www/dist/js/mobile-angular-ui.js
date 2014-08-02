6c0d3b5205a4437f78090763c89d082a69f34597
Travis Russi <travis@russigroup.com>
2014-08-01 21:09:49 -0400

    updated head for cordova

--- cordova/www/dist/js/mobile-angular-ui.js
+++ cordova/www/dist/js/mobile-angular-ui.js
@@ -306,128 +306,6 @@
 		
 })( this, this.overthrow );
 
-/*! Overthrow. An overflow:auto polyfill for responsive design. (c) 2012: Scott Jehl, Filament Group, Inc. http://filamentgroup.github.com/Overthrow/license.txt */
-(function( w, o, undefined ){
-
-	// o is overthrow reference from overthrow-polyfill.js
-	if( o === undefined ){
-		return;
-	}
-
-	// Easing can use any of Robert Penner's equations (http://www.robertpenner.com/easing_terms_of_use.html). By default, overthrow includes ease-out-cubic
-	// arguments: t = current iteration, b = initial value, c = end value, d = total iterations
-	// use w.overthrow.easing to provide a custom function externally, or pass an easing function as a callback to the toss method
-	o.easing = function (t, b, c, d) {
-		return c*((t=t/d-1)*t*t + 1) + b;
-	};
-
-	// tossing property is true during a programatic scroll
-	o.tossing = false;
-
-	// Keeper of intervals
-	var timeKeeper;
-
-	/* toss scrolls and element with easing
-
-	// elem is the element to scroll
-	// options hash:
-		* left is the desired horizontal scroll. Default is "+0". For relative distances, pass a string with "+" or "-" in front.
-		* top is the desired vertical scroll. Default is "+0". For relative distances, pass a string with "+" or "-" in front.
-		* duration is the number of milliseconds the throw will take. Default is 100.
-		* easing is an optional custom easing function. Default is w.overthrow.easing. Must follow the easing function signature
-
-	*/
-	o.toss = function( elem, options ){
-		o.intercept();
-		var i = 0,
-			sLeft = elem.scrollLeft,
-			sTop = elem.scrollTop,
-			// Toss defaults
-			op = {
-				top: "+0",
-				left: "+0",
-				duration: 50,
-				easing: o.easing,
-				finished: function() {}
-			},
-			endLeft, endTop, finished = false;
-
-		// Mixin based on predefined defaults
-		if( options ){
-			for( var j in op ){
-				if( options[ j ] !== undefined ){
-					op[ j ] = options[ j ];
-				}
-			}
-		}
-
-		// Convert relative values to ints
-		// First the left val
-		if( typeof op.left === "string" ){
-			op.left = parseFloat( op.left );
-			endLeft = op.left + sLeft;
-		}
-		else {
-			endLeft = op.left;
-			op.left = op.left - sLeft;
-		}
-		// Then the top val
-		if( typeof op.top === "string" ){
-
-			op.top = parseFloat( op.top );
-			endTop = op.top + sTop;
-		}
-		else {
-			endTop = op.top;
-			op.top = op.top - sTop;
-		}
-
-		o.tossing = true;
-		timeKeeper = setInterval(function(){
-			if( i++ < op.duration ){
-				elem.scrollLeft = op.easing( i, sLeft, op.left, op.duration );
-				elem.scrollTop = op.easing( i, sTop, op.top, op.duration );
-			}
-			else{
-				if( endLeft !== elem.scrollLeft ){
-					elem.scrollLeft = endLeft;
-				} else {
-					// if the end of the vertical scrolling has taken place
-					// we know that we're done here call the callback
-					// otherwise signal that horizontal scrolling is complete
-					if( finished ) {
-						op.finished();
-					}
-					finished = true;
-				}
-
-				if( endTop !== elem.scrollTop ){
-					elem.scrollTop = endTop;
-				} else {
-					// if the end of the horizontal scrolling has taken place
-					// we know that we're done here call the callback
-					if( finished ) {
-						op.finished();
-					}
-					finished = true;
-				}
-
-				o.intercept();
-			}
-		}, 1 );
-
-		// Return the values, post-mixin, with end values specified
-		return { top: endTop, left: endLeft, duration: o.duration, easing: o.easing };
-	};
-
-	// Intercept any throw in progress
-	o.intercept = function(){
-		clearInterval( timeKeeper );
-		o.tossing = false;
-	};
-
-})( this, this.overthrow );
-
 /**
  * @preserve FastClick: polyfill to remove click delays on browsers with touch UIs.
  *
@@ -1331,10 +1209,17 @@ angular.module("mobile-angular-ui.directives.capture", [])
 .directive("contentFor", [
   "CaptureService", function(CaptureService) {
     return {
-      link: function(scope, elem, attrs) {
-        CaptureService.setContentFor(attrs.contentFor, elem.html(), scope);
-        if (attrs.duplicate == null) {
-          elem.remove();
+      compile: function(tElem, tAttrs) {
+        var rawContent = tElem.html();
+        if(tAttrs.duplicate == null) {
+          // no need to compile anything!
+          tElem.html("");
+        }
+        return function postLink(scope, elem, attrs) {
+          CaptureService.setContentFor(attrs.contentFor, rawContent, scope);
+          if (attrs.duplicate == null) {
+            elem.remove();
+          }
         }
       }
     };
@@ -1558,19 +1443,13 @@ angular.module('mobile-angular-ui.directives.forms', [])
       changeExpr: "@ngChange",
       disabled: "@"
     },
-    template: "<div class='switch'><div class='switch-handle'></div></div>",
+    template: "<div class='switch' ng-class='{active: model}'><div class='switch-handle'></div></div>",
     link: function(scope, elem, attrs) {
-
-      function updateClass(model) {
-        if (model) { elem.addClass('active'); } else { elem.removeClass('active'); }
-      }
-
-      updateClass(scope.model);
 
       elem.on('click tap', function(){
         if (attrs.disabled == null) {
           scope.model = !scope.model;
-          updateClass(scope.model);
+          scope.$apply();
 
           if (scope.changeExpr != null) {
             scope.$parent.$eval(scope.changeExpr);
@@ -1582,7 +1461,6 @@ angular.module('mobile-angular-ui.directives.forms', [])
     }
   };
 });
-
 angular.module('mobile-angular-ui.directives.navbars', [])
 
 .directive('navbarAbsoluteTop', function() {
@@ -1607,31 +1485,34 @@ angular.module('mobile-angular-ui.directives.navbars', [])
 angular.module('mobile-angular-ui.directives.overlay', []).directive('overlay', [
   "$compile", function($compile) {
     return {
-      link: function(scope, elem, attrs) {
-        var active = "";
-        var body = elem.html();
-        var id = attrs.overlay;
+        compile: function(tElem, tAttrs) {
+            var rawContent = tElem.html();
+            return function postLink(scope, elem, attrs) {
+                var active = "";
+                var body = rawContent;
+                var id = attrs.overlay;
 
-        if (attrs["default"] != null) {
-          var active = "default='" + attrs["default"] + "'";
+                if (attrs["default"] != null) {
+                  var active = "default='" + attrs["default"] + "'";
+                }
+
+                var html = "<div class=\"overlay\" id=\"" + id + "\" toggleable " + active + " parent-active-class=\"overlay-in\" active-class=\"overlay-show\">\n  <div class=\"overlay-inner\">\n    <div class=\"overlay-background\"></div>\n    <a href=\"#" + id + "\" toggle=\"off\" class=\"overlay-dismiss\">\n      <i class=\"fa fa-times-circle-o\"></i>\n    </a>\n    <div class=\"overlay-content\">\n      <div class=\"overlay-body\">\n        " + body + "\n      </div>\n    </div>\n  </div>\n</div>";
+                elem.remove();
+
+                var sameId = angular.element(document.getElementById(id));
+
+                if (sameId.length > 0 && sameId.hasClass('overlay')) {
+                  sameId.remove();
+                }
+
+                body = angular.element(document.body);
+                body.prepend($compile(html)(scope));
+
+                if (attrs["default"] === "active") {
+                   body.addClass('overlay-in');
+                }
+            }
         }
-
-        var html = "<div class=\"overlay\" id=\"" + id + "\" toggleable " + active + " parent-active-class=\"overlay-in\" active-class=\"overlay-show\">\n  <div class=\"overlay-inner\">\n    <div class=\"overlay-background\"></div>\n    <a href=\"#" + id + "\" toggle=\"off\" class=\"overlay-dismiss\">\n      <i class=\"fa fa-times-circle-o\"></i>\n    </a>\n    <div class=\"overlay-content\">\n      <div class=\"overlay-body\">\n        " + body + "\n      </div>\n    </div>\n  </div>\n</div>";
-        elem.remove();
-
-        var sameId = angular.element(document.getElementById(id));
-
-        if (sameId.length > 0 && sameId.hasClass('overlay')) {
-          sameId.remove();
-        }
-
-        body = angular.element(document.body);
-        body.prepend($compile(html)(scope));
-
-        if (attrs["default"] === "active") {
-           body.addClass('overlay-in');
-        }
-      }
     };
   }
 ]);
